1338ec10fd52697eb37b554e7b32ea83388f6c4c
T. Tran <ttran375@my.centennialcollege.ca>
2024-05-15 19:33:33 +0000

    Wed, May 15, 2024, 7:33 PM +00:00

--- WEEK2-2/my-react-app/src/App.js
+++ WEEK2-2/my-react-app/src/App.js
@@ -1,24 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 
+const items = [
+  "1 cup unsalted butter",
+  "1 cup crunchy peanut butter",
+  "1 cup brown sugar",
+  "1 cup white sugar",
+  "2 eggs",
+  "2.5 cups all purpose flour",
+  "1 teaspoon baking powder",
+  "0.5 teaspoon salt",
+];
 function IngredientsList() {
-return React.createElement(
-"ul",
-{ className: "ingredients" },
-React.createElement("li", null, "1 cup unsalted butter"),
-
-React.createElement("li", null, "1 cup crunchy peanut butter"),
-React.createElement("li", null, "1 cup brown sugar"),
-React.createElement("li", null, "1 cup white sugar"),
-React.createElement("li", null, "2 eggs"),
-React.createElement("li", null, "2.5 cups all purpose flour"),
-React.createElement("li", null, "1 teaspoon baking powder"),
-React.createElement("li", null, "0.5 teaspoon salt")
-);
+  return React.createElement(
+    "ul",
+    { className: "ingredients" },
+    items.map((ingredient, i) =>
+      React.createElement("li", { key: i }, ingredient)
+    )
+  );
 }
+
 ReactDOM.render(
-React.createElement(IngredientsList, null, null),
-document.getElementById("root")
+  React.createElement(IngredientsList, { items }, null),
+  document.getElementById("root")
 );
 
 export default IngredientsList;
