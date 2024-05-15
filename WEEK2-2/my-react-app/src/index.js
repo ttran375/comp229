98725268c3c2b891cef4b9250aeb8274bf17f7b8
T. Tran <ttran375@my.centennialcollege.ca>
2024-05-15 19:00:56 +0000

    Wed, May 15, 2024, 7:00 PM +00:00

--- WEEK2-2/my-react-app/src/index.js
+++ WEEK2-2/my-react-app/src/index.js
@@ -1,17 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+//import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
-
+//import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
+const items = [
+"1 cup unsalted butter",
+"1 cup crunchy peanut butter",
+"1 cup brown sugar",
+"1 cup white sugar",
+"2 eggs",
+"2.5 cups all purpose flour",
+"1 teaspoon baking powder",
+"0.5 teaspoon salt"
+];
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+ <React.StrictMode>
+ <App items={items} />
+ </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
