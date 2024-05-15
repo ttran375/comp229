3bbad208936c6f681f1bba791d32ed45a83a8691
T. Tran <ttran375@my.centennialcollege.ca>
2024-05-15 19:16:42 +0000

    Wed, May 15, 2024, 7:16 PM +00:00

--- WEEK1/lab-session-1/lib.js
+++
@@ -1,6 +0,0 @@
-export function halfOf(x) {
-    return x / 2;
-}
-export function multiply(x, y) {
-    return x * y;
-}
--- WEEK1/lab-session-1/main.js
+++
@@ -1,4 +0,0 @@
-import {halfOf, multiply} from './lib.js';
-//
-console.log(halfOf(84));
-console.log(multiply(21, 2));
