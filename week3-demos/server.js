bf257369ccbcdbc5c1cddc00933521c60a5ab712
T. Tran <ttran375@my.centennialcollege.ca>
2024-05-22 19:45:49 +0000

    Wed, May 22, 2024, 7:45 PM +00:00

--- week3-demos/server.js
+++ week3-demos/server.js
@@ -1,11 +1,25 @@
-const connect = require("connect");
+const connect = require('connect');
 const app = connect();
 
-function helloWorld(req, res, next) {
-  res.setHeader("Content-Type", "text/plain");
-  res.end("Hello World");
+function logger(req, res, next) {
+    console.log(req.method, req.url);
+    next();
 }
 
-app.use(helloWorld);
-app.listen(3000);
-console.log("Server running at http://localhost:3000/");
+function helloWorld(req, res, next) {
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello World');
+}
+
+function goodbyeWorld(req, res, next) {
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Goodbye World');
+}
+
+app.use(logger);
+app.use('/hello', helloWorld);
+app.use('/goodbye', goodbyeWorld);
+
+app.listen(3000, () => {
+    console.log('Server running at http://localhost:3000/');
+});
