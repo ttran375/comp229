271c72b670f3b42c31e7a954787600518e5137d2
T. Tran <ttran375@my.centennialcollege.ca>
2024-05-22 19:38:59 +0000

    Wed, May 22, 2024, 7:38 PM +00:00

--- week3-demos/server.js
+++ week3-demos/server.js
@@ -1,7 +1,11 @@
-const http = require('http');
-http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello, world!\n');
-}).listen(3000);
+const connect = require("connect");
+const app = connect();
 
-console.log('Server running at http://localhost:3000/');
+function helloWorld(req, res, next) {
+  res.setHeader("Content-Type", "text/plain");
+  res.end("Hello World");
+}
+
+app.use(helloWorld);
+app.listen(3000);
+console.log("Server running at http://localhost:3000/");
