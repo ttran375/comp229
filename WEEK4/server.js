796a7693fa1a81b8671ef24eb374f3f9b2b477d8
ttran375 <ttran375@my.centennialcollege.ca>
2024-05-27 14:54:49 +0000

    Mon, May 27, 2024, 2:54 PM +00:00

--- WEEK4/server.js
+++ WEEK4/server.js
@@ -1,25 +1,10 @@
-const connect = require('connect');
-const app = connect();
+var express = require('express');
+var app = express();
 
-function logger(req, res, next) {
-    console.log(req.method, req.url);
-    next();
-}
-
-function helloWorld(req, res, next) {
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello World');
-}
-
-function goodbyeWorld(req, res, next) {
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Goodbye World');
-}
-
-app.use(logger);
-app.use('/hello', helloWorld);
-app.use('/goodbye', goodbyeWorld);
-
-app.listen(3000, () => {
-    console.log('Server running at http://localhost:3000/');
+app.use('/', function (req, res) {
+    res.send('Hello World');
 });
+
+app.listen(3000);
+console.log('Server running at http://localhost:3000/');
+module.exports = app;
