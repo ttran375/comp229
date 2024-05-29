8b33b135f1d1c02344b343eefd6ab188cecd6de7
ttran375 <ttran375@my.centennialcollege.ca>
2024-05-29 19:27:29 +0000

    Wed, May 29, 2024, 7:27 PM +00:00

--- mern_Skeleton/server/express.js
+++ mern_Skeleton/server/express.js
@@ -1,11 +1,14 @@
-import config from './../config/config'
-import app from './express'
-app.get("/", (req, res) => {
-    res.json({ message: "Welcome to User application." });
-});
-app.listen(config.port, (err) => {
-    if (err) {
-        console.log(err)
-    }
-    console.info('Server started on port %s.', config.port)
-})
+import express from 'express'
+import bodyParser from 'body-parser'
+import cookieParser from 'cookie-parser'
+import compress from 'compression'
+import cors from 'cors'
+import helmet from 'helmet'
+const app = express()
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(cookieParser())
+app.use(compress())
+app.use(helmet())
+app.use(cors())
+export default app
